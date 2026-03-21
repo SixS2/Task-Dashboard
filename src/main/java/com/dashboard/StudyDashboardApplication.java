9b3f23f78984ee138800493abb503c02b05118a4
SixS2 <sixs2@users.noreply.github.com>
2026-03-20 21:05:44 -0300

    pt-BR

--- src/main/java/com/dashboard/StudyDashboardApplication.java
+++ src/main/java/com/dashboard/StudyDashboardApplication.java
@@ -2,9 +2,16 @@ package com.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class StudyDashboardApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyDashboardApplication.class, args);
